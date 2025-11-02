a544d94b5cb87c1ca6873b02f04677675c89dffb
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2025-11-01 21:52:58 -0300

    Modal finalizado com sucesso!

--- src/app/shared/modal/modal.ts
+++ src/app/shared/modal/modal.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from "@angular/material/button";
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-modal',
-  imports: [],
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, MatChipsModule],
   templateUrl: './modal.html',
   styleUrl: './modal.scss',
 })
